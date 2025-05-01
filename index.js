50ef6100a24ea46901cea41aec805677f7219e3c
sadaan <ansarisadaan72@gmail.com>
2025-05-02 01:19:02 +0530

    "initial commit"

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 const app = express()
-dotenv.config;
-const port = process.env.PORT || 4000;
+dotenv.config();
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send('Hello World! this is testing')
