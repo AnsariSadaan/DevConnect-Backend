b707f1471432f1a3403994fe37880b23d57b25a7
sadaan <ansarisadaan72@gmail.com>
2025-05-04 11:48:16 +0530

    "express setup and routes with global err handler"

---
+++ src/app.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import cors from 'cors';
+import userRouter from './routes/auth.routes.js';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+app.use(cookieParser());
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.static('public'));
+
+
+app.use('/api/v1/users', userRouter)
+
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+
+    // ADDED A CHECK TO AVOID DUPLICATE RESPONSE
+    if (res.headersSent) {
+        return next(err);
+    }
+
+    res.status(statusCode).json({
+        statusCode,
+        success: false,
+        message,
+        errors: err.errors || [],
+        stack: process.env.NODE_ENV === "production" ? undefined : err.stack
+    });
+});
+
+export default app;
