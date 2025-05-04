01281bac73c5e9393afbd1280b5c024de09dd467
sadaan <ansarisadaan72@gmail.com>
2025-05-04 11:36:11 +0530

    "user model schema"

---
+++ src/models/user.model.js
@@ -0,0 +1,96 @@
+import mongoose, { Schema } from 'mongoose';
+import validator from 'validator';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            minLength: 4,
+            maxLength: 50
+        },
+        lastName: {
+            type: String
+        },
+        emailId: {
+            type: String,
+            lowercase: true,
+            required: true,
+            unique: true,
+            trim: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error("Invalid email address: " + value);
+                }
+            }
+        },
+        password: {
+            type: String,
+            required: true,
+            validate(value) {
+                if (!validator.isStrongPassword(value)) {
+                    throw new Error("Enter a strong password: " + value);
+                }
+            }
+        },
+        age: {
+            type: Number,
+            min: 18,
+        },
+        gender: {
+            type: String,
+            enum: {
+                values: ["male", "female", "other"],
+                message: `{VALUE} is not valid gender type`,
+            }
+        },
+        isPremium: {
+            type: Boolean,
+            default: false,
+        },
+        membershipType: {
+            type: String,
+        },
+        photoUrl: {
+            type: String,
+            default: "https://geographyandyou.com/images/user-profile.png",
+            validate(value) {
+                if (!validator.isURL(value)) {
+                    throw new Error('Invalid Photo URL: ', +value);
+                }
+            }
+        },
+        about: {
+            type: String,
+            default: "thi is default about of the user!",
+        },
+        skills: {
+            type: [String],
+        },
+    },
+    { timestamps: true }
+)
+
+
+userSchema.methods.getJWT = async function () {
+    const user = this;
+    const token = await jwt.sign({_id: user._id}, process.env.SECRET_KEY, {
+        expiresIn: process.env.ExpiresIn
+    });
+    return token;
+};
+
+userSchema.methods.validatePassword = async function (passwordInputByUser) {
+    const user = this;
+    const passwordHash =  user.password;
+    const isPasswordValid = await bcrypt.compare(
+        passwordInputByUser,
+        passwordHash
+    );
+    return isPasswordValid;
+}
+
+
+export const User = mongoose.model('User', userSchema)
