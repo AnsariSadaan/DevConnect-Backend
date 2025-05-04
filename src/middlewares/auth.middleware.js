d12e8ac85a161c2b206607bdf13e6394cb9337cb
sadaan <ansarisadaan72@gmail.com>
2025-05-04 11:39:40 +0530

    "jwt middleware"

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,19 @@
+import  jwt  from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { AsyncHandler } from "../utils/AsyncHandler.js";
+
+export const verifyJwt = AsyncHandler(async (req, _, next)=> {
+        const {token} = req.cookies;
+        if(!token){
+            throw new ApiError(401, "Please Login in to access this resource");
+        }
+        const decodedObj = await jwt.verify(token, process.env.JWT_SECRET)
+        const {_id} = decodedObj;
+        const user = await User.findById(_id);
+        if(!user){
+            throw new ApiError(401, "user not found");
+        } 
+        req.user = user;
+        next();
+})
