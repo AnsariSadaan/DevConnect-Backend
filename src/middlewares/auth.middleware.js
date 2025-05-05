61dec1eaf1d15b94abf9bd3e0fd9691ac63d94ff
sadaan <ansarisadaan72@gmail.com>
2025-05-05 22:13:42 +0530

    added req in paramter and removed await

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,12 +3,12 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { AsyncHandler } from "../utils/AsyncHandler.js";
 
-export const verifyJwt = AsyncHandler(async (req, _, next)=> {
+export const verifyJwt = AsyncHandler(async (req, res, next)=> {
         const {token} = req.cookies;
         if(!token){
             throw new ApiError(401, "Please Login in to access this resource");
         }
-        const decodedObj = await jwt.verify(token, process.env.JWT_SECRET)
+        const decodedObj = jwt.verify(token, process.env.JWT_SECRET)
         const {_id} = decodedObj;
         const user = await User.findById(_id);
         if(!user){
