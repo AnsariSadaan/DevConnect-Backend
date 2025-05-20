bdb791def58b624e24ee5de7f84cd4898ff98178
sadaan <ansarisadaan72@gmail.com>
2025-05-21 00:03:48 +0530

    "connection schema"

---
+++ src/models/connectionRequest.model.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+import { ApiError } from '../utils/ApiError.js';
+
+const connectionRequestSchema = new mongoose.Schema({
+    fromUserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    toUserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: {
+            values: ["ignored", "interested", "accepted", "rejected"],
+            message: `{VALUE} is incorrect status type`,
+        },
+    }
+}, { timestamps: true });
+
+
+connectionRequestSchema.index({fromUserId:1, toUserId:1});
+connectionRequestSchema.pre("save", function (next) {
+    const connectionRequest = this;
+    // Check if the fromUserId is same as toUserId
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+        throw new ApiError(403, "cannot send connection request to yourself")
+    }
+})
+
+export const ConnectionRequest = mongoose.model("connectionRequest", connectionRequestSchema)
