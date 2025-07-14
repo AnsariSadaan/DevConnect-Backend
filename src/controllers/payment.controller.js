a569687c29fc5b3f82ffa9919d1c9950bb5a4db8
sadaan <ansarisadaan72@gmail.com>
2025-07-14 11:34:33 +0530

    payment functionality

---
+++ src/controllers/payment.controller.js
@@ -0,0 +1,22 @@
+import { AsyncHandler } from "../utils/AsyncHandler.js";
+import razorpayInstance from '../utils/Razorpay.js' 
+
+const Payment = AsyncHandler(async (req, res)=> {
+    const order = await razorpayInstance.orders.create({
+        "amount": 50000, //it means 500 rps it consider as paisa not in rupees 
+        "currency": "INR",
+        "receipt": "receipt#1",
+        "notes": {
+            "firstName": "value3",
+            "lastName": "value2",
+            "membershipType": "silver"
+        },
+    });
+    // save in my db
+    console.log(order);
+    // return back my order details to frontend 
+    return res.status(200).json({order})
+})
+
+
+export default Payment;
