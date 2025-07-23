09e797c4d84f3acf96d5081408964be27f63783b
Sadaan Ansari <saddan.ansari@slashrtc.com>
2025-07-23 21:56:48 +0530

    commented

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 // import  Payment  from "../controllers/payment.controller.js";
-import { verifyJwt } from "../middlewares/auth.middleware.js";
+// import { verifyJwt } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 // router.route('/payment/create').post(verifyJwt, Payment);
