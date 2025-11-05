c30c90676ef73dd196eeb242429c14d2177792c8
Sadaan Ansari <ansarisadaan72@gmail.com>
2025-11-05 22:31:33 +0530

    [added]::: chat configuration

--- src/config/socketConnection.js
+++ src/config/socketConnection.js
@@ -1,5 +1,6 @@
 import { Server } from 'socket.io';
 import crypto from 'crypto';
+import { Chat } from '../models/chat.model.js';
 
 const getSecretRoomId = (userId, targetUserId) => {
   return crypto
@@ -29,10 +30,33 @@ const initializeSocket = (server) => {
       socket.join(roomId);
     });
 
-    socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
-      const roomId = getSecretRoomId(userId, targetUserId);
-      console.log(firstName + " " + text);
-      io.to(roomId).emit("messageReceived", {firstName, text});
+    socket.on("sendMessage", async ({ firstName, lastName, userId, targetUserId, text }) => {
+      // save message 
+      try {
+        const roomId = getSecretRoomId(userId, targetUserId);
+        console.log(firstName + " " + text);
+        let chat = await Chat.findOne({
+          participants: { $all : [userId, targetUserId]},
+          
+        });
+        if (!chat) {
+          chat = new Chat({
+            participants: [userId, targetUserId],
+            messages: [],
+          }) 
+        }
+
+        chat.messages.push({
+          senderId: userId,
+          text
+        })
+
+        await chat.save();
+        io.to(roomId).emit("messageReceived", {firstName, lastName, text});
+
+      } catch (error) {
+        console.log(error)
+      }
     });
 
     socket.on("disconnect", () => {
